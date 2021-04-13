91cee0720401823801ab07114654a9f3a2aa7171
Stefan <filimonstefanmihai@gmail.com>
2021-04-13 14:19:42 +0300

    idk man, the site looks like it has more style + changed some of the buttons

--- haticus/components/index/hero.tsx
+++ haticus/components/index/hero.tsx
@@ -80,6 +80,7 @@ const Hero = () => {
               aria-label='Global'>
               <div className='flex items-center flex-1 md:inset-y-0 md:left-0'>
                 <div className='flex items-center justify-between w-full md:w-auto'>
+                <div className='DogeGardenLogoB'>
                   <Link href='/'>
                     <a>
                       <span className='sr-only'>{settings.name}</span>
@@ -90,6 +91,7 @@ const Hero = () => {
                       />
                     </a>
                   </Link>
+                  </div>
                   <div className='-mr-2 flex items-center md:hidden'>
                     <button
                       type='button'
@@ -213,8 +215,13 @@ const Hero = () => {
           <div className='mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6'>
             <div className='text-center'>
               <h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
+              <div className='providingToolsText'>
                 <span className='block'>Providing tools</span>
+              </div>
+
+              <div className='forDogehouseText'>
                 <span className='block text-dc'>for DogeHouse.tv</span>
+              </div>
               </h1>
               <p className='mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl'>
                 DogeGarden is the parent organization behind numerous tools
@@ -229,11 +236,13 @@ const Hero = () => {
             <div className='flex-1 w-full bg-ghc'></div>
           </div>
           <div className='max-w-7xl mx-auto px-4 sm:px-6'>
+          <div className='previewImage'>
             <img
               className='relative rounded-lg shadow-lg'
               src='/images/preview.png'
               alt='App screenshot'
             />
+          </div>
           </div>
         </div>
       </div>
@@ -243,6 +252,7 @@ const Hero = () => {
             Responsible for great community tools
           </h2>
           <div className='mt-8 flex flex-col lg:flex-row items-center justify-center gap-y-8 lg:gap-y-0 lg:gap-x-16'>
+          <div className='buttonForApi'>
             <a
               href='https://github.com/dogegarden/dogehouse-api'
               target='_blank'>
@@ -252,7 +262,8 @@ const Hero = () => {
                 alt='DogeHouse API Logo'
               />
             </a>
-
+            </div>
+          <div className='buttonForJS'>
             <a
               href='https://github.com/dogegarden/dogehouse.js'
               target='_blank'>
@@ -262,7 +273,8 @@ const Hero = () => {
                 alt='DogeHouse.js Logo'
               />
             </a>
-
+          </div>
+          <div className='buttonForStats'>
             <a
               href='https://github.com/dogegarden/dogegarden-stats'
               target='_blank'>
@@ -272,6 +284,7 @@ const Hero = () => {
                 alt='DogeGarden Stats Logo'
               />
             </a>
+            </div>
           </div>
         </div>
       </div>
